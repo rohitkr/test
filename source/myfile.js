a72ac423b9013b0ad2f4c22084674b2c58a644f2
Rohit Kumar <rohit@fusioncharts.com>
2014-01-16 13:47:06 +0530

    Added a new function to subtract two numbers given as a parameter.

--- source/myfile.js
+++ source/myfile.js
@@ -4,7 +4,14 @@
  * @requires myfile3
  * @export sample-file2.js
  */
-/* jshint trailing: true, browser: false, undef: true, jquery: true */
+
+/** 
+ * jshint trailing: true,
+ * browser: false,
+ * undef: true,
+ * jquery: true 
+ */
+
 /** @todo A sample todo block which needs to be removed */
 var MY_GLOBAL;
 function addNumbers () {
@@ -16,8 +23,24 @@ function addNumbers () {
 	
     return sum;
 }
+
+/**
+ * Function returns subtraction of two numbers
+ * @params num1 Number
+ * @params num2 Number
+ * @return result Number
+ */
+/** @todo A sample todo block which needs to be removed */
+function subtractNumbers (num1, num2) {
+    var numbers = arguments,
+        result = num1 - num2;
+
+    return sum;
+}
+
 /** @todo A sample todo block which needs to be removed */
 var a = $('div'),
 b = navigator.userAgent;
 
 addNumbers(5, 6, 8, 9);
+subtractNumbers(500, 200);
