10e425cbf39b53b1ed8ad51ca93e6e8ea2f7c860
Rohit Kumar <rohitkumar@LPMB-RohitK.local>
2016-08-19 11:51:02 +0530

    Test modification

--- ci/build/index.js
+++ ci/build/index.js
@@ -17,50 +17,50 @@ var gulp = require('gulp');
 
 
 
-	gulp.src('source/*.js')
-	.pipe(closureCompiler({
-            js_output_file: 'test.js',
-            externs: [
-              // 'develop/ci/build/externs.js'
-            ],
-            extra_annotation_name: [
-                'export',
-                'todo',
-                'note',
-                'callback',
-                'var',
-                'section',
-                'alias',
-                'mixin',
-                'mixer',
-                'group',
-                'groupInfo'
-            ],
-            // only_closure_dependencies: false,
-            charset: 'utf8',
-            jscomp_error: [
-                'accessControls',
-                'ambiguousFunctionDecl',
-                'checkDebuggerStatement',
-                'checkRegExp',
-                'const',
-                'constantProperty',
-                'deprecated',
-                'externsValidation',
-                'fileoverviewTags',
-                'globalThis',
-                'nonStandardJsDocs',
-                'uselessCode',
-                'visibility'
-            ],
-            language_in: 'ECMASCRIPT3',
-            jscomp_off: [
-                'es5Strict',
-                'misplacedTypeAnnotation',
-                // 'checkVars',
-                'nonStandardJsDocs'
-            ],
-            jscomp_warning: 'suspiciousCode'
-	}))
-	.pipe(gulp.dest('dist/js'));
+// gulp.src('source/*.js')
+// .pipe(closureCompiler({
+//         js_output_file: 'test.js',
+//         externs: [
+//           // 'develop/ci/build/externs.js'
+//         ],
+//         extra_annotation_name: [
+//             'export',
+//             'todo',
+//             'note',
+//             'callback',
+//             'var',
+//             'section',
+//             'alias',
+//             'mixin',
+//             'mixer',
+//             'group',
+//             'groupInfo'
+//         ],
+//         // only_closure_dependencies: false,
+//         charset: 'utf8',
+//         jscomp_error: [
+//             'accessControls',
+//             'ambiguousFunctionDecl',
+//             'checkDebuggerStatement',
+//             'checkRegExp',
+//             'const',
+//             'constantProperty',
+//             'deprecated',
+//             'externsValidation',
+//             'fileoverviewTags',
+//             'globalThis',
+//             'nonStandardJsDocs',
+//             'uselessCode',
+//             'visibility'
+//         ],
+//         language_in: 'ECMASCRIPT3',
+//         jscomp_off: [
+//             'es5Strict',
+//             'misplacedTypeAnnotation',
+//             // 'checkVars',
+//             'nonStandardJsDocs'
+//         ],
+//         jscomp_warning: 'suspiciousCode'
+// }))
+// .pipe(gulp.dest('dist/js'));
 
